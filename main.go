66d1eaa0036a223c7a092b07f410de9870a46b43
Stephen Gruzin <stephengrzn@gmail.com>
2024-06-27 00:58:28 -0700

    A simple websocket server with a simple home page.

--- main.go
+++ main.go
@@ -1,7 +1,80 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gorilla/websocket"
+)
+
+func getRoot(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("got / request\n")
+	io.WriteString(w, "This is my website!\n")
+}
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
+
+var connections []*websocket.Conn
+
+func RemoveIndex(s []*websocket.Conn, index int) []*websocket.Conn {
+	return append(s[:index], s[index+1:]...)
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	fmt.Println("New Client", conn.RemoteAddr().String())
+	connections = append(connections, conn)
+
+	for {
+		messageType, p, err := conn.ReadMessage()
+		if err != nil {
+			indexToRemove := -1
+			for index, connection := range connections {
+				if connection.RemoteAddr().String() == conn.RemoteAddr().String() {
+					indexToRemove = index
+				}
+			}
+			if indexToRemove > -1 {
+				connections = RemoveIndex(connections, indexToRemove)
+			}
+			fmt.Println("Client Disconnected", conn.RemoteAddr().String())
+			return
+		}
+
+		for _, connection := range connections {
+			if connection.RemoteAddr().String() != conn.RemoteAddr().String() {
+				if err := connection.WriteMessage(messageType, p); err != nil {
+					log.Println(err)
+					return
+				}
+			}
+		}
+	}
+}
 
 func main() {
-	fmt.Println("Hello World!")
+	http.Handle("/", http.FileServer(http.Dir("./static")))
+	http.HandleFunc("/test", getRoot)
+	http.HandleFunc("/socket", handler)
+
+	err := http.ListenAndServe(":3333", nil)
+
+	if errors.Is(err, http.ErrServerClosed) {
+		fmt.Printf("server closed\n")
+	} else if err != nil {
+		fmt.Printf("error starting server: %s\n", err)
+		os.Exit(1)
+	}
 }
